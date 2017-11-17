5a1803f6eee9b659d51a0767d05a8dd37b1fee7e
boblee200508 <bob_lee200508@163.com>
2017-11-17 21:26:20 +0800

    My first independence coding progamme
    get!

--- 167-two-sum-II/c/solution_bob.c
+++ 167-two-sum-II/c/solution_bob.c
@@ -4,6 +4,18 @@ int main () {
     int x[] = {1 , 2 , 3 , 5 };
     int target = 3;
     while(1){
+        if(x[0] + x[1] == target) {
+            printf("x[0] , x[1]");
+            break;
+        }
+        if(x[0] + x[2] == target) {
+            printf("x[0] , x[2]");
+            break;
+        }
+        if(x[0] + x[3] == target) {
+            printf("x[0] , x[3]");
+            break;
+        }
         if(x[1] + x[2] == target) {
             printf("x[1] , x[2]");
             break;
@@ -12,20 +24,8 @@ int main () {
             printf("x[1] , x[3]");
             break;
         }
-        if(x[1] + x[4] == target) {
-            printf("x[1] , x[4]");
-            break;
-        }
         if(x[2] + x[3] == target) {
             printf("x[2] , x[3]");
-            break;
-        }
-        if(x[2] + x[4] == target) {
-            printf("x[2] , x[4]");
-            break;
-        }
-        if(x[3] + x[4] == target) {
-            printf("x[3] , x[4]");
             break;
         }
     }
